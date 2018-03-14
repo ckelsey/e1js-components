86710a1840502341221382a12bcef4384e45cbad
chris kelsey <chris@ckcreates.com>
2018-03-14 18:11:20 -0500

    build

--- src/edit/edit.js
+++ src/edit/edit.js
@@ -40,9 +40,27 @@ class Edit {
             sel.addRange(range)
         })
 
+        var utf8Encode = (unicodeString)=> {
+            if (typeof unicodeString != 'string') throw new TypeError('parameter ‘unicodeString’ is not a string');
+            const utf8String = unicodeString.replace(
+                /[\u0080-\u07ff]/g,  // U+0080 - U+07FF => 2 bytes 110yyyyy, 10zzzzzz
+                function (c) {
+                    var cc = c.charCodeAt(0);
+                    return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f);
+                }
+            ).replace(
+                /[\u0800-\uffff]/g,  // U+0800 - U+FFFF => 3 bytes 1110xxxx, 10yyyyyy, 10zzzzzz
+                function (c) {
+                    var cc = c.charCodeAt(0);
+                    return String.fromCharCode(0xe0 | cc >> 12, 0x80 | cc >> 6 & 0x3F, 0x80 | cc & 0x3f);
+                }
+            );
+            return utf8String;
+        }
+
         var save = ()=>{
             var value = this.el.querySelector(".edit-content").textContent
-            value = value ? value.trim().replace(/[^\x20-\x7E]+/g, '') : ``
+            value = value ? utf8Encode(value.trim()) : ``
 
             E1.setModel(null, `@E1EditService.editors.${this.editor.name}.editing`, false)
             E1.setModel(this.el, "content", value)
